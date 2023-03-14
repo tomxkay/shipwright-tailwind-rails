c3df3d863d1e5108b091f83b1186c419e90c4ba4
Thomas Kay <tomxkay@pm.me>
2023-03-14 10:28:49 -0400

    add view component to config application

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require "view_component/storybook/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
