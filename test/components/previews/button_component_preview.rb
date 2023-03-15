c1e17d867632ed9d182459603f5f6c768af079be
Thomas Kay <tomxkay@pm.me>
2023-03-15 14:06:59 -0400

    refactor styles

--- test/components/previews/button_component_preview.rb
+++ test/components/previews/button_component_preview.rb
@@ -4,7 +4,7 @@ class ButtonComponentPreview < ViewComponent::Preview
   def default(type: :primary)
     type = type.to_sym if type
 
-    render(ButtonComponent.new(type: type)) { 'Button' }
+    render(ButtonComponent.new(type:)) { 'Button' }
   end
 
   def primary
