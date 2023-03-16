2c5e1da4052047e95e1630878012b788cd33d8ca
Thomas Kay <tomxkay@pm.me>
2023-03-16 15:58:23 -0400

    add description for button story

--- test/components/stories/button_component_stories.rb
+++ test/components/stories/button_component_stories.rb
@@ -1,8 +1,9 @@
 class ButtonComponentStories < ViewComponent::Storybook::Stories
   title 'Atoms/Button'
 
-  control :variant, as: :select, options: %w[fill outline ghost], default: 'fill'
-  control :label, as: :text, default: 'Button'
+  control :variant, as: :select, options: %w[fill outline ghost], default: 'fill',
+                    description: 'selects styling of button.'
+  control :label, as: :text, default: 'Button', description: 'sets the label of the button.'
   def default(variant: :fill, label: 'Button')
     render(ButtonComponent.new(variant:).with_content(label))
   end
