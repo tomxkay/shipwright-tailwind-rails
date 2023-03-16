a2e7d3db6b3cd3128495e68619720a62000999f1
Thomas Kay <tomxkay@pm.me>
2023-03-16 13:44:54 -0400

    update card component to new syntax

--- app/components/card_component.rb
+++ app/components/card_component.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class CardComponent < ViewComponent::Base
-  attr_reader :image, :isStandard, :text, :title
+  attr_reader :image, :is_standard, :text, :title
 
-  def initialize(image: '', isStandard: false, text: '', title: '')
+  def initialize(image: '', is_standard: false, text: '', title: '')
     @image = image
-    @isStandard = isStandard
+    @is_standard = is_standard
     @text = text
     @title = title
   end
