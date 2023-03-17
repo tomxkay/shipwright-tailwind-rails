8012788fcc4cc4e68bb1e7cea58566db493a93be
Thomas Kay <tomxkay@pm.me>
2023-03-17 11:12:15 -0400

    use fallback defaults

--- test/components/stories/button_component_stories.rb
+++ test/components/stories/button_component_stories.rb
@@ -3,7 +3,7 @@ class ButtonComponentStories < ViewComponent::Storybook::Stories
 
   control :variant, as: :select, options: %w[fill outline ghost], default: 'fill',
                     description: 'selects styling of button.'
-  control :label, as: :text, default: 'Button', description: 'sets the label of the button.'
+  control :label, as: :text, description: 'sets the label of the button.'
   def button(variant: :fill, label: 'Button')
     render(ButtonComponent.new(variant:).with_content(label))
   end
